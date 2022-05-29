b19474ad4c7306c4ac84328a95cff5342f50329d
sk211 <sujanmajumder211@gmail.com>
2022-05-29 08:53:53 +0600

    readio_api

---
+++ back-end/index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require("cors")
+const app = express();
+const ObjectId = require("mongodb").ObjectId;
+const port = process.env.PORT || 5000
+// const port =  5000
+require('dotenv').config();
+
+// middleware 
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSOWRD}@cluster0.4bmge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+async function run() {
+
+    try {
+        await client.connect();
+
+        const packagesCollection = client.db("redio_db").collection("signUp");
+        const orderCollection = client.db("redio_db").collection("create");
+
+
+        // post api 
+        app.post('/create', async (req, res) => {
+            const package = req.body
+            console.log("hit thw poar api", package);
+            const result = await packagesCollection.insertOne(package)
+            console.log(result);
+            res.json(result)
+        })
+
+
+
+        // get api 
+        app.get('/reade', async (req, res) => {
+            const cursor = packagesCollection.find({})
+            const products = await cursor.toArray();
+            res.send(products)
+
+        })
+
+
+        // delete event
+
+        app.delete("/deleteOrder/:id", async (req, res) => {
+            console.log(req.params.id);
+            const result = await orderCollection.deleteOne({
+                _id: ObjectId(req.params.id),
+            });
+            res.send(result);
+        });
+
+
+    }
+    finally {
+        // await cllient.close()
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send("Assignment server is running")
+})
+
+
+
+app.listen(port, () => {
+    console.log("Server Running Port", port);
+})
+
+    ;
